1f474e5cc3b4ff344377d34c1f362ce236542ad9
Benjamen King <brk.3rdof3kings@gmail.com>
2017-12-15 03:38:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,37 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const liElts = document.querySelectorAll('.ranked-list li');
+  
+  liElts.forEach( liElt =>
+    liElt.innerHTML = (parseInt(liElt.innerHTML) + n).toString()
+  );
+}
+
+function deepestChild() {
+  let deepest;
+  const queue = [document.querySelector('div#grand-node')];
+  
+  function addToQueue(node) {
+    queue.push(node);
+  }
+  
+  while (queue.length > 0) {
+    const children = Array.from(queue[0].children);
+    if (children.length > 0) {
+      children.forEach(addToQueue);
+    }
+    if (queue.length === 1) {
+      deepest = queue[0];
+    }
+    queue.shift();
+  }
+  
+  return deepest;
+}
